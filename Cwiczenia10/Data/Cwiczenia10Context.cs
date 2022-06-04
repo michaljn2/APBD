19ecb3e9b7c82acaa2601d9dd638b3d90983afe3
Michał Januszewski s22882 <s22882@pjwstk.edu.pl>
2022-06-04 18:13:03 +0200

    Dodaje projekt

---
+++ Cwiczenia10/Data/Cwiczenia10Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Cwiczenia10.Models;
+
+namespace Cwiczenia10.Data
+{
+    public class Cwiczenia10Context : DbContext
+    {
+        public Cwiczenia10Context (DbContextOptions<Cwiczenia10Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Cwiczenia10.Models.Movie> Movie { get; set; }
+    }
+}
